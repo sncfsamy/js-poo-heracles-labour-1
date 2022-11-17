953fcb7c7c57087e79180d7b86c0013fbd331a80
sncfsamy <samy.lamiri@sncf.fr>
2022-11-17 11:30:06 +0100

    first comit

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+// Import stylesheets
+//import './style.css';
+
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+//const Fighter = require('./src/Fighter');
+
+const heracles = new Fighter('🧔 Héraclès', 20, 6);
+const nemean = new Fighter('🦁 Lion de Némée', 11, 13);
+
+function add(x) {
+    const newDiv = document.createElement("div");
+    newDiv.innerText = x;
+    document.querySelector("#game").appendChild(newDiv);
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+add(heracles.name + ' : ' + heracles.getLife());
+add(nemean.name + ' : ' + nemean.getLife());
+add("Le combat commencera dans 10sec...");
+let round = 1;
+function fight() {
+  add('🕛 Round n°' + round);
+  heracles.isAlive(() => add(heracles.fight(nemean)));
+  nemean.isAlive(() => add(nemean.fight(heracles)));
+  if (nemean.life === 0) {
+    add("🕛 Le combat s'est terminé au round n°" + round);
+    return;
+  } else {
+    round++;
+    setTimeout(fight, 1000);
+  }
+}
+setTimeout(fight, 10000);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,30 @@
+
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter {
+    constructor (name, strength, dexterity, life = MAX_LIFE) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = life;
+    }
+
+    getLife() {
+        return this.name + " 💙: " + Math.ceil(this.life);
+    }
+
+    fight(fighter) {
+        if (this.life == 0) return "";
+        let attack = Math.random() * this.strength;
+        attack = (attack - fighter.dexterity)>0 ? attack-fighter.dexterity: Math.random()>0.5? 1 : 0;
+        fighter.life -= (fighter.life-attack >=0)? attack : fighter.life;
+        return fighter.life > 0 ? this.name + "⚔️" + fighter.name + ": " + fighter.getLife() : "🏆" + this.name + " a gagné ! ("+this.getLife() +")\r\n" + "💀" + fighter.name + " est mort.";
+    }
+
+    isAlive(f) {
+        if (this.life) f();
+    }
+}
+
+//module.exports = Fighter;
